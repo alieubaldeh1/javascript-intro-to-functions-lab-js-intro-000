16a0c13aee7f95097a37eec479c0d0d198f7cb42
Alieu Baldeh <alieubaldeh@gmail.com>
2020-12-09 21:09:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,5 +20,7 @@ function sayHiToGrandma(string) {
   }
   else if (string === shout()) {
     return "YES INDEED"
-  } else if (string === "I love you, Grandma.")
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
