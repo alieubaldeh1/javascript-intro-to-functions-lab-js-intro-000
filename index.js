c1d39116c22beb8f03ca2ae768867dcdee473a3d
Alieu Baldeh <alieubaldeh@gmail.com>
2020-12-09 21:12:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === whisper()) {
+  if (string === string.toLowerCase() {
     return "I can't hear you!"
   }
-  else if (string === shout()) {
+  else if (string === string.toUpperCase() {
     return "YES INDEED"
   } else if (string === "I love you, Grandma.") {
     return "I love you, too."
